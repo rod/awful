1924be365693c8e06c544567354574acbdd37f1a
Rodney Dennis <rodneydennisjr@gmail.com>
2016-11-04 17:48:07 -0500

    defines commands

---
+++ cli.js
@@ -0,0 +1,34 @@
+const argv = require('minimist')(process.argv.slice(2));
+const pkg = require('./package.json');
+const awful = require('./');
+
+console.log(argv);
+
+if (argv.help) {
+  console.log([
+    'Usage',
+    '  $ awful',
+    '  miserable',
+    '',
+    '  $ awful --all',
+    '  lousy',
+    '  miserable',
+    '  ...',
+    '',
+    'Options',
+    '  --all  Get all the words instead of a random word',
+  ].join('\n'));
+  return;
+}
+
+if (argv.version) {
+  console.log(pkg.version);
+  return;
+}
+
+if (argv.all) {
+  console.log(awful.words.join('\n'));
+  return;
+}
+
+console.log(awful());
--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-const argv = require('minimist')(process.argv.slice(2));
+'use strict';
+var uniqueRandomArray = require('unique-random-array');
+var words = require('./words.json');
 
-if (argv.help === true ) {
-  const help = '
-    Examples
-      $ awful
-      miserable
-
-      $ awful --all
-      lousy
-      miserable
-      ...
-
-    Options
-      --all Get all the words instead of a random word'
-
-  console.log(help);
-}
-console.dir(argv);
+module.exports = uniqueRandomArray(words);
+module.exports.words = words;
